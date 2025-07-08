d70ce7d95eefd2a11371730bebbf4b5fccecedaf
robbie <rtambunting@gmail.com>
2025-07-07 17:16:31 -0700

    Make homepage responsive

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,7 +9,7 @@ A fixed box in the bottom left of the screen where users can find Mamun's social
 
 function Socials() {
   return (
-    <div className="absolute bottom-4 left-4 flex flex-col gap-y-4 bg-(--mist-white) px-3 py-5">
+    <div className="absolute bottom-4 left-4 z-50 flex flex-col gap-y-4 bg-(--mist-white) px-3 py-5">
       <a href="" className="translate-y">
         <FaTiktok className="size-4" />
       </a>
