852501486b7e484199cd8056288117b1aae319c4
robbie <rtambunting@gmail.com>
2025-07-12 21:38:23 -0700

    Remove Juma'ah and Startup Jobs from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,13 +17,7 @@ function Header() {
       <div>
         <ul className="flex items-center justify-between gap-x-6">
           <li className="translate-y font-semibold">
-            <a href="#">Juma&apos;ah</a>
-          </li>
-          <li className="translate-y font-semibold">
             <a href="/cofoundr-matching">Co-Foundr Matching</a>
-          </li>
-          <li className="translate-y font-semibold">
-            <a href="#">Startup Jobs</a>
           </li>
           <li className="translate-y font-semibold">
             <a href="#">Mission</a>
