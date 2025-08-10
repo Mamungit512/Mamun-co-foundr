3eca0ef400e4909fa56941c48b5808fb78b71e80
robbie <rtambunting@gmail.com>
2025-08-09 22:33:57 -0700

    Implement react-hook-form to send post request to Resend api endpoint

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-export async function GET() {
+export async function POST() {
   const resend = new Resend(process.env.RESEND_API_KEY);
 
   try {
     const { data } = await resend.emails.send({
       from: "mamun@mamuncofoundr.com",
       to: ["mamun@mamuncofoundr.com"],
-      subject: "Testing resend! Please delete",
+      subject: "Contact Us Form Submission",
       html: "<h1>Hello this is a test</h1>",
     });
     return NextResponse.json({ data });
