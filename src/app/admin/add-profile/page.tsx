70c955b5605f82bd81bdcbc6ea03cb0d963676a2
robbie <rtambunting@gmail.com>
2025-08-12 12:25:38 -0700

    Add isUserAdmin service + admin authorized route to create new profiles

--- src/app/admin/add-profile/page.tsx
+++ src/app/admin/add-profile/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+// import { redirect } from "next/navigation";
+import OnboardingComponent from "@/app/onboarding/page";
 
-function page() {
-  return (
-    <div>page</div>
-  )
+import { isUserAdmin } from "@/features/auth/authService";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function AdminAddProfilePage() {
+  // Get user session server-side via Clerk
+  const { userId, getToken } = await auth();
+
+  if (!userId) {
+    // redirect("/sign-in");
+    return <div>Unauthorized. Please Sign In</div>;
+  }
+
+  const token = await getToken();
+
+  if (!token) {
+    console.error("Invalid token");
+    return <div>Unauthorized. Please Sign In</div>
+  }
+
+  const admin = await isUserAdmin(userId, token);
+
+  if (!admin) return <p>Access denied. Admins only.</p>;
+
+  return <OnboardingComponent />;
 }
-
-export default page
