a2a92632e4de69a3df4a5b5d407530ec2ea390c7
Hacer Kılıç <hacerkilic194@gmail.com>
2025-12-18 23:11:03 +0300

    added FAQ added to top

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -8,12 +8,18 @@ function FAQ() {
   const faqList = [
     {
       id: 1,
+      question: "Can I apply for funding ?",
+      answer:
+        "Founders will soon become eligible for Mamun ECC funding by actively building and demonstrating progress. Being clear about what they aim to create. Consistently engaging with other builders on the platform. Building trust over time through action.",
+    },
+    {
+      id: 2,
       question: "What is the Mamun Co-founder Match service?",
       answer:
         "Mamun Co-founder Match is a platform that connects aspiring Muslim entrepreneurs with complementary skills and a shared commitment to social impact.",
     },
     {
-      id: 2,
+      id: 3,
       question: "What are the benefits of using Mamun Co-founder Match?",
       answer: [
         "Save time and effort compared to traditional networking methods.",
@@ -23,62 +29,55 @@ function FAQ() {
       ],
     },
     {
-      id: 3,
+      id: 4,
       question: "Is this service only for Muslims living in a specific region?",
       answer:
         "No, Mamun Co-founder Matching is a global platform open to Muslim and people from all walks of life  worldwide.",
     },
     {
-      id: 4,
+      id: 5,
       question: "What is the Matching process like ?",
       answer:
         "The algorithm considers factors like your skills, experience, desired venture focus, and  Islamic values to connect you with compatible co-founders.",
     },
     {
-      id: 5,
+      id: 6,
       question: "Can I influence who I get matched with?",
       answer:
         "Yes, you can set preferences based on specific skills, experience, and venture type  you're interested in.",
     },
     {
-      id: 6,
+      id: 7,
       question: "What happens if I don't get matched with anyone?",
       answer:
         "Mamun is constantly growing its user base. You can refine your profile or preferences  and keep an eye out for new matches as they become available.",
     },
     {
-      id: 7,
+      id: 8,
       question:
         "How does Mamun ensure the safety and security of my information?",
       answer:
         "Mamun employs robust security measures to protect user data. We recommend you review our privacy policy for detailed information.",
     },
     {
-      id: 8,
+      id: 9,
       question: "How do I know the person I'm matched with is trustworthy?",
       answer:
         "Mamun uses background checks (where applicable) and encourages transparent  communication between potential co-founders. However, it's ultimately your responsibility  to assess your comfort level with any potential partner.",
     },
     {
-      id: 9,
+      id: 10,
       question:
         "How does Mamun consider Islamic values in the matching process?",
       answer:
         "The matching algorithm may consider factors like preferred work styles and communication styles, work industry and location.",
     },
     {
-      id: 10,
+      id: 11,
       question:
         "Can I connect with co-founders who want to launch Sharia-compliant ventures?",
       answer:
         "Yes, you can specify your interest in Sharia-compliant ventures in your profile and  preferences. Mamun might also explore integrating with Islamic financial institutions in  the future to facilitate access to Sharia-compliant funding.",
-    },
-      {
-      id: 11,
-      question:
-        "Can I apply for funding ?",
-      answer:
-        "Founders will soon become eligible for Mamun ECC funding by actively building and demonstrating progress. Being clear about what they aim to create. Consistently engaging with other builders on the platform. Building trust over time through action.",
     },
   ];
 
