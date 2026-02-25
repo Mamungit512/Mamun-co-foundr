964ebafc3cc9647e9b29be57ee8bb3e6336a80c7
Hacer Kılıç <hacerkilic194@gmail.com>
2026-02-25 23:07:52 +0300

    fix Elfsight bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -181,7 +181,7 @@ function Page() {
 
             {isSignedIn && (
               <motion.div
-                className="mt-8 flex w-full justify-center px-4 sm:mt-10 md:mt-24 md:mb-20"
+                className="mt-8 flex w-full justify-center px-4 sm:mt-10 md:mt-12 md:mb-12"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8, delay: 2.2, ease: "easeOut" }}
